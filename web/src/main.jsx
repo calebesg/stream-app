ab8d950a6692a5aa36157bc2bac90d5bb5b6a7fc
CalebeSG <calebeguimaraes10@gmail.com>
2022-06-04 10:33:40 -0300

    config redux devtools

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
@@ -8,7 +8,8 @@ import reducers from './reducers';
 
 import './main.css';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
